1eb613389c6267890be31cc61f050f86c7d3ef65
Siddhartha Chowdhury <sidc.117@gmail.com>
2021-02-02 15:15:12 +0530

    first commit

---
+++ mergepdf.py
@@ -0,0 +1,14 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+def mergepdf(pdf_list):
+	merger_object = PyPDF2.PdfFileMerger()
+	for pdf in pdf_list:
+		print(pdf)
+		merger_object.append(pdf)
+	merger_object.write('COMBINED.pdf')
+	
+	
+mergepdf(inputs)
---
+++ splitpdf.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os, sys
+
+
+def split_pdf(path):
+	fname = os.path.splitext(os.path.basename(path))[0]
+	
+	pdf = PdfFileReader(path)
+	for page in range(pdf.getNumPages()):
+		writer_object = PdfFileWriter()
+		writer_object.addPage(pdf.getPage(page))
+		
+		output_pdf = '{}_page_{}.pdf'.format(fname, page+1)
+		with open(output_pdf, 'wb') as opt:
+			writer_object.write(opt)
+		print('PDF Split complete')
+		
+if __name__ == '__main__':
+	inputs = sys.argv[1]
+	split_pdf(inputs)
